44bd78c61c1cccaea7ac3674874c931b937404b7
Carmelo Sammarco <sammarcocarmelo@gmail.com>
2025-04-20 03:03:15 +0200

    fix

--- mcp-docker.py
+++ mcp-docker.py
@@ -2,7 +2,11 @@ import os
 import subprocess
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("mcp-docker")
+mcp = FastMCP(
+    name="Demo",
+    host="0.0.0.0",  # only used for SSE transport
+    port=8050,  # only used for SSE transport (set this to any port)
+)
 
 
 @mcp.tool()
@@ -10,5 +14,6 @@ async def multiply(a: float, b: float) -> float:
     """Multiply two numbers"""
     return a * b
 
+
 if __name__ == "__main__":
-    mcp.run(transport='stdio')
+    mcp.run(transport="stdio")  # or SSE protocol
